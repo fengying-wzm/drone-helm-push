1b7b860a240c27c8b6faef68baf1e418753ebe17
风影 <297083979@qq.com>
2019-11-08 15:02:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,12 @@
 
 set -o pipefail
 
-helm version
+CHART = "$PLUGIN_CHART"
+CERT = "$PLUGIN_CERT"
+USERNAME = "$PLUGIN_USERNAME"
+PASSWORD = "$PLUGIN_PASSWORD"
+REPO = "$PLUGIN_REPO"
+
+helm package $CHART
+
+helm push --ca-file $CERT --username $USERNAME --password $PASSWORD *.tgz $REPO
