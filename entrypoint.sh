cf9b59f4cc3d134dbe508fa820dbe660589e0b4f
风影 <297083979@qq.com>
2019-11-11 14:53:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,45 @@
-#! /bin/bash -eu
+#! /bin/bash
 
-set -o pipefail
+REPO="$PLUGIN_REPO"
+echo "REPO -> $REPO"
+if [ ! -n "$REPO" ]; then
+  echo "settings: REPO not found "
+  exit 1
+fi
 
-CHART = "$PLUGIN_CHART"
-CERT = "$PLUGIN_CERT"
-USERNAME = "$PLUGIN_USERNAME"
-PASSWORD = "$PLUGIN_PASSWORD"
-REPO = "$PLUGIN_REPO"
+CHART="$PLUGIN_CHART"
+echo "CHART -> $CHART"
+if [ ! -n "$CHART" ]; then
+  echo "settings: CHART not found "
+  exit 1
+fi
+
+CERT="$PLUGIN_CERT"
+echo "CERT -> $CERT"
+if [ ! -n "$REPO" ]; then
+  CERT=""
+else
+  echo -e "$CERT" > ./ca.crt
+  chmod 777 ./ca.crt
+  CERT="--ca-file ./ca.crt"
+fi
+
+USERNAME="$PLUGIN_USERNAME"
+echo "USERNAME -> $USERNAME"
+if [ ! -n "$USERNAME" ]; then
+  USERNAME=""
+else
+  USERNAME="--username $USERNAME"
+fi
+
+PASSWORD="$PLUGIN_PASSWORD"
+echo "PASSWORD -> $PASSWORD"
+if [ ! -n "$PASSWORD" ]; then
+  PASSWORD=""
+else
+  PASSWORD="--password $PASSWORD"
+fi
 
 helm package $CHART
 
-helm push --ca-file $CERT --username $USERNAME --password $PASSWORD *.tgz $REPO
+helm push $CERT $USERNAME $PASSWORD *.tgz $REPO
